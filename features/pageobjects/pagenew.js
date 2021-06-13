9b8d63953df8409c89bc99a11ac39b27168c396a
Darshan29292 <83182608+Darshan29292@users.noreply.github.com>
2021-06-13 12:44:38 +0530

    Update pagenew.js

--- features/pageobjects/pagenew.js
+++ features/pageobjects/pagenew.js
@@ -1,7 +1,7 @@
 /**
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
-*/
+*/ 
 export default class PageNew {
     /**
     * Opens a sub page of the page
